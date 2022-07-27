66355c4c9a8b6b06fc5d97a7665285446049c110
amalhotra40 <abhinav@culturebiosciences.com>
2022-07-27 09:36:37 -0400

    Update conf.py

--- vesselesque/docs/conf.py
+++ vesselesque/docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../vessel/"))
+sys.path.insert(0, os.path.abspath("../vesselesque/"))
 
 
 # -- Project information -----------------------------------------------------
