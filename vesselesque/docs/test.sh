7f7ff59077463e8059ec238f734be13958fbc50f
amalhotra40 <abhinav@culturebiosciences.com>
2022-07-27 14:22:40 -0400

    Update test.sh

--- vesselesque/docs/test.sh
+++ vesselesque/docs/test.sh
@@ -2,8 +2,8 @@
 asdf install python 3.8.10
 asdf global python 3.8.10
 asdf plugin add poetry
-asdf install poetry latest
-asdf global poetry latest
+asdf install poetry 1.1.12
+asdf global poetry 1.1.12
 poetry config virtualenvs.create true
 cd vesselesque
 cat pyproject.toml
